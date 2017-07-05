fc2fa38ff2ad24099a09c2698f48c26a35d502e6
kleendevos <kleen.devos@gmail.com>
2017-07-05 11:13:19 +0200

    oefening 05/07/17

---
+++ src/main/java/com/realdolmen/Fraction.java
@@ -0,0 +1,35 @@
+package com.realdolmen;
+
+/**
+ * Created by vdabcursist on 05/07/2017.
+ */
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int numinator () {
+        return numerator;
+    }
+
+    public int denomintor (){
+        return denominator;
+    }
+
+    @Override
+    public String toString() {
+        return (numerator + "/" + denominator);
+    }
+
+    private void simplify(){
+        int factor = Utilities.greatestCommonFactor(numerator,denominator);
+        numerator /= factor;
+        denominator /= factor;
+    }
+
+}
+
---
+++ src/test/java/com/realdolmen/Fraction.java
@@ -0,0 +1,10 @@
+package com.realdolmen;
+
+/**
+ * Created by vdabcursist on 05/07/2017.
+ */
+public class Fraction {
+
+
+
+}
